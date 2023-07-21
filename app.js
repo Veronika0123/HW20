9705310635417ea92105ec6e3feea5f5da713d90
Veronika0123 <veronikamokrii@gmail.com>
2023-07-21 19:16:25 +0300

    /

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-let number = +prompt('введите число');
-for (let i = 2; i <= number; i++) { 
-
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0); 
+let num = +prompt('введите число');
+if (num == 2 || num == 3 || num == 5 || num == 7 || num == 11 || num == 13 || num == 17 || num == 19 || num == 23 || num == 29 || num == 31 || num == 37 || num == 41 || num == 43 || num == 47 || num == 53 || num == 59 || num == 61 || num == 67  || num == 71 || num == 73 || num == 79 || num == 83  || num == 89 || num == 97){
+    alert('Простое')
+  }else{
+    alert('Не простое')
   }
-
-  alert( i ); 
-}
